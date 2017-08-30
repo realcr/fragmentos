fa227a168c48f9f0ccfee19108e5272075d37474
real <real@freedomlayer.org>
2017-08-30 17:57:48 +0300

    Initial implementation of split_message.

--- src/messages.rs
+++ src/messages.rs
@@ -2,12 +2,11 @@ extern crate crypto;
 extern crate reed_solomon;
 
 use self::reed_solomon::Encoder;
-
-
 use self::crypto::sha2::Sha256;
 use self::crypto::digest::Digest;
 
 use shares::{split_data, unite_data};
+
 /*
 Fragmentos message:
 
@@ -16,11 +15,15 @@ Fragmentos message:
 - shareIndex        [1 byte]
 - shareData         [variable amount of bytes]
 - errorCorrection   [8 bytes]
+
+`T := nonce8 || paddingCount || M || padding`
 */
 
+// Length of messageId (First 8 bytes of sha256 of the underlying T data):
 const MESSAGE_ID_LEN: usize = 8;
+// Length in bytes of the Reed Solomon error correcting code:
 const ECC_LEN: usize = 8;
-
+// Length in bytes of nonce in the beginning of the underlying T data:
 const NONCE_LEN: usize = 8;
 
 
@@ -35,14 +38,12 @@ fn max_message(max_datagram: usize) -> Result<usize,()> {
     }
 }
 
-/// Split a message m into a few fragmentos messages, to be sent to the destination.
+/// Split a message m into a few Fragmentos messages, to be sent to the destination.
 /// Could fail if message is too large.
 fn split_message(m: &[u8], nonce: &[u8; NONCE_LEN], max_datagram: usize) -> Result<Vec<Vec<u8>>,()> {
     if m.len() > max_message(max_datagram)? {
         return Err(())
     }
-
-    // `T := nonce8 || paddingCount || M || padding`
 
     let len_without_padding = MESSAGE_ID_LEN + 1 + m.len();
     let space_in_msg = max_datagram - (MESSAGE_ID_LEN + 1 + 1 + ECC_LEN);
@@ -64,35 +65,30 @@ fn split_message(m: &[u8], nonce: &[u8; NONCE_LEN], max_datagram: usize) -> Resu
     let mut hasher = Sha256::new();
     let output_len = hasher.output_bytes();
     if output_len < MESSAGE_ID_LEN {
-        // The result of the hash hash to at least as long as messageId.
+        // The result of the hash hash to at least as long as message_id.
         return Err(());
     }
     let mut sha256_hash = vec![0; output_len];
     hasher.input(&t);
     hasher.result(&mut sha256_hash);
-    let messageId = &sha256_hash[0..MESSAGE_ID_LEN];
+    let message_id = &sha256_hash[0..MESSAGE_ID_LEN];
 
     let data_shares = split_data(m, b as u8)?;
 
     let enc =  Encoder::new(ECC_LEN);
 
-    data_shares
+    Ok(data_shares
         .into_iter()
         .enumerate()
         .map(|(i, data_share)| {
             let mut fmessage = Vec::new();
-            fmessage.extend_from_slice(messageId);
+            fmessage.extend_from_slice(message_id);
             fmessage.push(b as u8);
             fmessage.push(i as u8);
             fmessage.extend_from_slice(&data_share.data);
-
-            // TODO: Add the error correcting code here:
-
-
+            let encoded = enc.encode(&fmessage);
+            fmessage.extend_from_slice(encoded.ecc());
+            fmessage
         }).collect::<Vec<Vec<u8>>>()
-
-    // pub fn split_data(data: &[u8], b: u8) -> Result<Vec<DataShare>,()> {
-
-
-
+    )
 }
