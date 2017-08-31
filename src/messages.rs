84ed26207397f24dc5f5ce9ea2700748de4583e6
real <real@freedomlayer.org>
2017-08-31 15:47:29 +0300

    Initial implementation of unite_message.

--- src/messages.rs
+++ src/messages.rs
@@ -38,10 +38,29 @@ fn max_message(max_datagram: usize) -> Result<usize,()> {
     }
 }
 
+/// Calculate a hash of T, to obtain messageId.
+fn calc_message_id(t: &[u8]) -> Result<Vec<u8>,()> {
+    let mut hasher = Sha256::new();
+    let output_len = hasher.output_bytes();
+    if output_len < MESSAGE_ID_LEN {
+        // The hash output be at least as long as message_id.
+        return Err(());
+    }
+    let mut sha256_hash = vec![0; output_len];
+    hasher.input(&t);
+    hasher.result(&mut sha256_hash);
+
+    // Take only the part we care about:
+    sha256_hash.truncate(MESSAGE_ID_LEN);
+    Ok(sha256_hash)
+}
+
 /// Split a message m into a few Fragmentos messages, to be sent to the destination.
 /// Could fail if message is too large.
 /// Returns a list of Fragmentos messages (As vectors) to be sent to the remote side.
-fn split_message(m: &[u8], nonce: &[u8; NONCE_LEN], max_datagram: usize) -> Result<Vec<Vec<u8>>,()> {
+fn split_message(m: &[u8], nonce: &[u8; NONCE_LEN], max_datagram: usize) 
+        -> Result<Vec<Vec<u8>>,()> {
+
     if m.len() > max_message(max_datagram)? {
         return Err(())
     }
@@ -63,20 +82,8 @@ fn split_message(m: &[u8], nonce: &[u8; NONCE_LEN], max_datagram: usize) -> Resu
         t.push(0);
     }
 
-    // Calculate sha256 of T:
-    let mut hasher = Sha256::new();
-    let output_len = hasher.output_bytes();
-    if output_len < MESSAGE_ID_LEN {
-        // The hash output be at least as long as message_id.
-        return Err(());
-    }
-    let mut sha256_hash = vec![0; output_len];
-    hasher.input(&t);
-    hasher.result(&mut sha256_hash);
-    let message_id = &sha256_hash[0..MESSAGE_ID_LEN];
-
+    let message_id = calc_message_id(&t)?;
     let data_shares = split_data(m, b as u8)?;
-
     let enc =  Encoder::new(ECC_LEN);
 
     Ok(data_shares
@@ -84,7 +91,7 @@ fn split_message(m: &[u8], nonce: &[u8; NONCE_LEN], max_datagram: usize) -> Resu
         .enumerate()
         .map(|(i, data_share)| {
             let mut fmessage = Vec::new();
-            fmessage.extend_from_slice(message_id);
+            fmessage.extend_from_slice(&message_id);
             fmessage.push(b as u8);
             fmessage.push(i as u8);
             fmessage.extend_from_slice(&data_share.data);
@@ -96,16 +103,25 @@ fn split_message(m: &[u8], nonce: &[u8; NONCE_LEN], max_datagram: usize) -> Resu
 }
 
 /// Reconstruct a message given a list of data shares.
-fn unite_message(message_id: &[u8; MESSAGE_ID_LEN], data_shares: &[DataShare]) -> Result<Vec<u8>,()> {
+fn unite_message(message_id: &[u8; MESSAGE_ID_LEN], data_shares: &[DataShare]) 
+        -> Result<Vec<u8>,()> {
+
     let b = data_shares.len();
     if b == 0 {
         return Err(());
     }
 
-    let t = unite_data(data_shares, b * data_shares[0].data.len())?;
+    let t = unite_data(data_shares)?;
 
-    assert!(false);
-    // TODO: Continue here.
+    // Make sure that the provided message_id matches the calculated message_id:
+    let c_message_id = calc_message_id(&t)?;
+    if message_id != &c_message_id[..] {
+        return Err(());
+    }
 
+    // let nonce = &t[0..NONCE_LEN];
+    let padding_count = t[NONCE_LEN] as usize;
+    let m = &t[NONCE_LEN + 1 .. t.len() - padding_count];
 
+    Ok(m.to_vec())
 }
