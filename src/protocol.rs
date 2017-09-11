7e3e237623c053174173090a9f3f256f8daeb0ee
real <real@freedomlayer.org>
2017-09-11 16:43:57 +0300

    Some work on RecvMsg poll function. Not done yet.

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,27 +1,95 @@
 extern crate futures;
 
-use self::futures::Future;
+use std::io;
+use std::mem;
+use self::futures::{Future, Poll, Async};
 
 use ::state_machine::{FragStateMachine};
 
-struct MsgReceiver<'a,T:'a,F:'a> {
+/// T: Return buffer, F: future, A: address type
+struct FragMsgReceiver<'a,T:'a,F:'a,A> 
+    where F: Future<Item=(T, usize, A), Error=io::Error>,
+          T: AsMut<[u8]> {
+
     frag_state_machine: FragStateMachine,
     recv_dgram: &'a Fn(T) -> F,
 }
 
-struct RecvMsg {
+
+struct RecvMsg<'a,T:'a,F:'a,A>
+    where F: Future<Item=(T, usize, A), Error=io::Error>,
+          T: AsMut<[u8]> {
+    state: RecvState<'a,T,F,A>,
 }
 
-impl<'a,T,F> MsgReceiver<'a,T,F> {
-    fn new(recv_dgram: &'a Fn(T) -> F) -> Self 
-        where T: AsMut<[u8]>, F: Future {
+enum RecvState<'a,T:'a,F:'a,A> 
+    where F: Future<Item=(T, usize, A), Error=io::Error>,
+          T: AsMut<[u8]> {
+    Reading {
+        msg_receiver: FragMsgReceiver<'a,T,F,A>,
+        buf: T,
+    },
+    Done,
+}
 
-        MsgReceiver {
+impl<'a,T,F,A> Future for RecvMsg<'a,T,F,A>
+    where F: Future<Item=(T, usize, A), Error=io::Error>,
+          T: AsMut<[u8]> {
+
+    type Item = (FragMsgReceiver<'a,T,F,A>, Vec<u8>, A);
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<Self::Item, io::Error> {
+
+        let got_message = false;
+        while !got_message {
+            let (buf, n, address) = match self.state {
+                RecvState::Reading {ref msg_receiver, buf } => {
+                    let fdgram = (*msg_receiver.recv_dgram)(buf);
+                    match fdgram.poll() {
+                        Ok(Async::Ready(t)) => t,
+                        Ok(Async::NotReady) => return Ok(Async::NotReady),
+                        Err(e) => return Err(e),
+                    }
+                },
+                RecvState::Done => panic!("Polling RecvMsg after it's done"),
+            };
+
+        }
+
+        // After we reconstructed a full message:
+        // TODO:
+
+        match mem::replace(&mut self.state, RecvState::Done) {
+            RecvState::Reading {msg_receiver, buf } => {
+                Ok(Async::Ready
+            },
+            RecvState::Done => panic!("Invalid state"),
+        }
+
+        // self.msg_receiver.recv_msg(
+    }
+}
+
+impl<'a,T,F,A> FragMsgReceiver<'a,T,F,A> 
+    where F: Future<Item=(T, usize, A), Error=io::Error>,
+          T: AsMut<[u8]> {
+
+    fn new(recv_dgram: &'a Fn(T) -> F) -> Self
+        where T: AsMut<[u8]>, F: Future<Item=(T, usize, A),Error=io::Error>  {
+
+        FragMsgReceiver {
             frag_state_machine: FragStateMachine::new(),
             recv_dgram,
         }
     }
 
-    fn recv_msg(&mut self) -> RecvMsg {
+    fn recv_msg(self, buf: T) -> RecvMsg<'a,T,F,A> {
+        RecvMsg {
+            state: RecvState::Reading {
+                msg_receiver: self,
+                buf,
+            },
+        }
     }
 }
