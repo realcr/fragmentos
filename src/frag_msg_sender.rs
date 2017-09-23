1a5daffe892f0017f6388348424c99d0f62316e1
real <real@freedomlayer.org>
2017-09-23 13:11:05 +0300

    Added test_frag_msg_sender_basic.

--- src/frag_msg_sender.rs
+++ src/frag_msg_sender.rs
@@ -67,3 +67,62 @@ where
         })
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    extern crate tokio_core;
+
+    use super::*;
+    use std::time::{Instant, Duration};
+
+    use self::rand::{StdRng, Rng};
+    use self::tokio_core::reactor::Core;
+
+    use ::state_machine::FragStateMachine;
+
+    #[test]
+    fn test_frag_msg_sender_basic() {
+        let orig_message = b"This is some message to be split";
+        // A maximum size of underlying datagram:
+        const MAX_DGRAM_LEN: usize = 22;
+        const ADDRESS: u32 = 0x12345678;
+
+        let mut sent_messages = Vec::new();
+
+        let seed: &[_] = &[1,2,3,4,5];
+        let rng: StdRng = rand::SeedableRng::from_seed(seed);
+
+        {
+            let send_dgram = |buff: Vec<u8>, addr| {
+                assert_eq!(addr, ADDRESS);
+                sent_messages.push(buff.clone());
+                future::ok::<_,io::Error>(buff)
+            };
+
+            let fms = FragMsgSender::new(send_dgram, MAX_DGRAM_LEN, rng);
+            let send_msg_fut = fms.send_msg(orig_message,ADDRESS);
+
+            let mut core = Core::new().unwrap();
+           
+            let (_fms, _orig_message) = core.run(send_msg_fut).unwrap();
+        }
+
+        // Feed a Fragmentos state machine with the sent messages:
+        let mut fsm = FragStateMachine::new();
+        let mut cur_inst = Instant::now();
+
+        let b = (sent_messages.len() + 1) / 2;
+        for i in 0 .. b - 1 {
+            assert_eq!(fsm.received_frag_message(&sent_messages[i], cur_inst), None);
+            fsm.time_tick(cur_inst);
+            cur_inst += Duration::new(0,5);
+        }
+
+        // Take the last fragment (From the end):
+        let united = fsm.received_frag_message(&sent_messages[sent_messages.len() - 1], cur_inst).unwrap();
+        assert_eq!(united, orig_message);
+
+    }
+}
+
