ba90671ffb9fb00d7d4aae900176de6014d6a460
real <real@freedomlayer.org>
2017-09-23 17:55:05 +0300

    Changed FragMsgReceiver to be a Stream wrapping a Stream.

--- src/frag_msg_receiver.rs
+++ src/frag_msg_receiver.rs
@@ -5,217 +5,90 @@ use std::mem;
 use std::time::Instant;
 use std::marker::PhantomData;
 
-use self::futures::{Future, Poll, Async};
+use self::futures::{Stream, Future, Poll, Async};
 
 use ::state_machine::{FragStateMachine};
-use ::messages::max_message;
 
-trait Dgramer<A>
-where
-    Self: Sized,
-{
-    fn recv_dgram<T,F>(self, T) -> F
-    where 
-        T: AsMut<[u8]>,
-        F: Future<Item=(Self, T, usize, A), Error=io::Error>;
-
-    fn send_dgram<T,F>(self, T) -> F 
-    where 
-        T: AsRef<[u8]>,
-        F: Future<Item=(Self, T), Error=io::Error>;
-}
-
-struct FragMessenger<A,D,Q> 
-where
-    D: Dgramer<A>,
+pub struct FragMsgReceiver<A,R,Q>
+where 
+    R: Stream<Item=(Vec<u8>, A), Error=io::Error>,
     Q: FnMut() -> Instant,
 {
-    dgramer: D,
     frag_state_machine: FragStateMachine,
+    recv_stream: R,
     get_cur_instant: Q,
-    max_dgram_len: usize,
     phantom_a: PhantomData<A>,
 }
 
-enum ReadingBuff<A,D,F> 
+
+impl<A,R,Q> FragMsgReceiver<A,R,Q>
 where
-    D: Dgramer<A>,
-    F: Future<Item=(D,Vec<u8>, usize, A), Error=io::Error>,
-{
-    Empty,
-    TempBuff(Vec<u8>),
-    ReadFuture(F),
-}
-
-struct ReadingState<A,D,Q,B,F>
-where 
-    D: Dgramer<A>,
-    Q: FnMut() -> Instant,
-    B: AsMut<[u8]>,
-    F: Future<Item=(D, Vec<u8>, usize, A), Error=io::Error>,
-{
-    frag_messenger: FragMessenger<A,D,Q>,
-    res_buff: B,
-    reading_buff: ReadingBuff<A,D,F>,
-}
-
-enum RecvState<A,D,Q,B,F>
-where 
-    D: Dgramer<A>,
-    Q: FnMut() -> Instant,
-    B: AsMut<[u8]>,
-    F: Future<Item=(D, Vec<u8>, usize, A), Error=io::Error>,
-{
-    Reading(ReadingState<A,D,Q,B,F>),
-    Done,
-}
-
-pub struct RecvMsg<A,D,Q,B,F>
-where 
-    D: Dgramer<A>,
-    Q: FnMut() -> Instant,
-    B: AsMut<[u8]>,
-    F: Future<Item=(D,Vec<u8>, usize, A), Error=io::Error>,
-{
-    state: RecvState<A,D,Q,B,F>,
-}
-
-
-impl<A,D,Q,B,F> Future for RecvMsg<A,D,Q,B,F>
-where 
-    D: Dgramer<A>,
-    Q: FnMut() -> Instant,
-    B: AsMut<[u8]>,
-    F: Future<Item=(D,Vec<u8>, usize, A), Error=io::Error>,
-{
-
-    // FragMsgReceiver, buffer, num_bytes, address
-    type Item = (FragMessenger<A,D,Q>, (B, usize, A));
-    type Error = io::Error;
-
-    fn poll(&mut self) -> Poll<Self::Item, io::Error> {
-        let (msg, address) = {
-            let reading_state = match self.state {
-                RecvState::Done => panic!("polling RecvMsg after it's done."),
-                RecvState::Reading(ref mut reading_state) => reading_state,
-            };
-
-            let total_msg: Vec<u8>;
-            let last_address: A;
-
-            loop {
-                let mut fdgram = match mem::replace(
-                    &mut reading_state.reading_buff, ReadingBuff::Empty) {
-
-                    ReadingBuff::Empty => panic!("Invalid state for reading_buff."),
-                    ReadingBuff::TempBuff(temp_buff) =>
-                        reading_state.frag_messenger.dgramer.recv_dgram(temp_buff),
-
-                    ReadingBuff::ReadFuture(fdgram) => fdgram,
-                };
-
-                // Try to obtain a message:
-                let (temp_buff, n ,address) = match fdgram.poll() {
-                    Ok(Async::Ready(t)) => t,
-                    Ok(Async::NotReady) => {
-                        mem::replace(&mut reading_state.reading_buff,
-                                     ReadingBuff::ReadFuture(fdgram));
-                        return Ok(Async::NotReady);
-                    },
-                    Err(e) => {
-                        mem::replace(&mut reading_state.reading_buff,
-                                     ReadingBuff::ReadFuture(fdgram));
-                        return Err(e);
-                    }
-                };
-                // Obtain current time:
-                let cur_instant = (reading_state.frag_msg_receiver.get_cur_instant)();
-
-                // Possibly clean up some old message fragments:
-                // TODO: This could be inefficient. In the future we might need to make time_tick
-                // work faster or add some kind of conditional mechanism to run this code only once
-                // in a while, instead of running it for every incoming message.
-                reading_state.frag_msg_receiver.frag_state_machine.time_tick(cur_instant);
-
-                // Add fragment to state machine, possibly reconstructing a full message:
-               
-                let msg_res = reading_state.frag_msg_receiver
-                    .frag_state_machine.received_frag_message(
-                        &temp_buff[0..n], cur_instant);
-
-                mem::replace(&mut reading_state.reading_buff,
-                             ReadingBuff::TempBuff(temp_buff));
-
-                match msg_res {
-                    Some(msg) => {
-                        // We got a full message. 
-                        // We break outside of the loop.
-                        total_msg = msg;
-                        last_address = address;
-                        break;
-                    }
-                    None => {},
-                };
-            }
-            (total_msg, last_address)
-        };
-
-        // We have a full message:
-        match mem::replace(&mut self.state, RecvState::Done) {
-            RecvState::Reading(mut reading_state) => {
-
-                // Make sure that we have enough room to write to buffer:
-                if reading_state.res_buff.as_mut().len() < msg.len() {
-                    panic!("Destination buffer too short!");
-                }
-
-                reading_state.res_buff.as_mut()[0 .. msg.len()].copy_from_slice(&msg);
-
-                let msg_item = (reading_state.res_buff, msg.len(), address);
-                Ok(Async::Ready((reading_state.frag_msg_receiver, msg_item)))
-            }
-            RecvState::Done => panic!("Invalid state"),
-        }
-    }
-}
-
-
-impl<A,D,Q> FragMessenger<A,D,Q>
-where
-    D: Dgramer<A>,
+    R: Stream<Item=(Vec<u8>, A), Error=io::Error>,
     Q: FnMut() -> Instant,
 {
-    pub fn new(dgramer: D, get_cur_instant: Q, max_dgram_len: usize) -> Self {
-        FragMessenger {
-            dgramer,
+    pub fn new(recv_stream: R, get_cur_instant: Q) -> Self {
+        FragMsgReceiver {
             frag_state_machine: FragStateMachine::new(),
+            recv_stream,
             get_cur_instant,
-            max_dgram_len,
             phantom_a: PhantomData,
         }
     }
+}
 
-    pub fn recv_msg<B,F>(self, mut res_buff: B) -> RecvMsg<A,D,Q,B,F> 
-    where
-        B: AsMut<[u8]>,
-        F: Future<Item=(D,Vec<u8>, usize, A), Error=io::Error>,
-    {
-        let max_dgram_len = self.max_dgram_len;
-        if res_buff.as_mut().len() < max_message(max_dgram_len).unwrap() {
-            panic!("res_buff is too short to hold any Fragmentos message.");
-        }
 
-        RecvMsg {
-            state: RecvState::Reading(
-                ReadingState {
-                    frag_messenger: self,
-                    res_buff,
-                    reading_buff: ReadingBuff::TempBuff(vec![0; max_dgram_len]),
+impl<A,R,Q> Stream for FragMsgReceiver<A,R,Q>
+where 
+    R: Stream<Item=(Vec<u8>, A), Error=io::Error>,
+    Q: FnMut() -> Instant,
+{
+    type Item = (Vec<u8>, A);
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<Option<Self::Item>, io::Error> {
+
+        let total_msg: Vec<u8>;
+        let last_address: A;
+
+        loop {
+            let (dgram, address) = match self.recv_stream.poll() {
+                Ok(Async::Ready(Some((dgram, address)))) => (dgram, address),
+                Ok(Async::Ready(None)) => return Ok(Async::Ready(None)),
+                Ok(Async::NotReady) => return Ok(Async::NotReady),
+                Err(e) => return Err(e),
+            };
+
+            // Obtain current time:
+            let cur_instant = (self.get_cur_instant)();
+
+            // Possibly clean up some old message fragments:
+            // TODO: This could be inefficient. In the future we might need to make time_tick
+            // work faster or add some kind of conditional mechanism to run this code only once
+            // in a while, instead of running it for every incoming message.
+            self.frag_state_machine.time_tick(cur_instant);
+
+            // Add fragment to state machine, possibly reconstructing a full message:
+           
+            let msg_res = self.frag_state_machine.received_frag_message(
+                    &dgram, cur_instant);
+
+            match msg_res {
+                Some(msg) => {
+                    // We got a full message. 
+                    // We break outside of the loop.
+                    total_msg = msg;
+                    last_address = address;
+                    break;
                 }
-            ),
+                None => {},
+            };
         }
+
+        // We have a full message:
+        Ok(Async::Ready(Some((total_msg, last_address))))
     }
 }
+
 
 #[cfg(test)]
 mod tests {
