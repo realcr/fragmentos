36416768a6e54a3b35559909103745e5ac8c7015
real <real@freedomlayer.org>
2017-09-26 20:12:49 +0300

    Wrote basic Sink->Stream Frag test using mpsc::channel. Only one Fragmentos message is received.

--- tests/sender_receiver.rs
+++ tests/sender_receiver.rs
@@ -3,15 +3,15 @@ extern crate futures;
 extern crate tokio_core;
 extern crate fragmentos;
 
-use std::collections::VecDeque;
 use std::io;
-use std::sync::mpsc;
+use futures::sync::mpsc;
 
 use std::time::Instant;
 
 use rand::StdRng;
 
-use futures::{stream, Stream, Sink, Poll, StartSend, AsyncSink, Async};
+use futures::{stream, Future, Stream, Sink, Poll, StartSend, AsyncSink, Async};
+use futures::task::Task;
 
 use fragmentos::frag_msg_receiver::FragMsgReceiver;
 use fragmentos::frag_msg_sender::FragMsgSender;
@@ -21,52 +21,75 @@ use self::tokio_core::reactor::Core;
 // A maximum size of underlying datagram:
 const MAX_DGRAM_LEN: usize = 22;
 
+/*
 
 struct MockSink<T> {
-    sender: mpsc::SyncSender<T>,
+    sender: mpsc::Sender<(T, Task)>,
 }
 
 struct MockStream<T> {
-    receiver: mpsc::Receiver<T>,
+    receiver: mpsc::Receiver<(T, Task)>,
 }
 
 /// Create a pair of mock Sink and Stream.
 /// Every message sent through Sink will arrive at Stream.
-fn sink_stream_pair<T>(bound: usize) -> (MockSink<T> ,MockStream<T>) {
-    let (sender, receiver) = mpsc::sync_channel(bound);
+fn sink_stream_pair<T>(buffer: usize) -> (MockSink<T> ,MockStream<T>) {
+    channel(buffer)
+    let (sender, receiver) = mpsc::channel(buffer);
     (MockSink {sender}, MockStream {receiver})
 }
 
-impl<T> Sink for MockSink<T> {
+impl<T> Sink for MockSink<T> 
+where 
+    T: std::fmt::Debug,
+{
+
     type SinkItem = T;
     type SinkError = ();
 
     fn start_send(&mut self, item: Self::SinkItem) 
         -> StartSend<Self::SinkItem, Self::SinkError> {
-        match self.sender.try_send(item) {
+        println!();
+        println!(">> start_send");
+        let ret = match self.sender.try_send((item, futures::task::current())) {
             Ok(()) => Ok(AsyncSink::Ready),
             Err(mpsc::TrySendError::Disconnected(_)) => Err(()),
-            Err(mpsc::TrySendError::Full(item)) => Ok(AsyncSink::NotReady(item)),
-        }
+            Err(mpsc::TrySendError::Full((item, task))) => Ok(AsyncSink::NotReady(item)),
+        };
+        println!("ret = {:?}",ret);
+        ret
     }
 
     fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
+        println!();
+        println!(">> poll_complete");
         Ok(Async::Ready(()))
     }
 }
 
-impl<T> Stream for MockStream<T> {
+impl<T> Stream for MockStream<T> 
+where
+    T: std::fmt::Debug,
+{
     type Item = T;
     type Error = ();
 
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
-        match self.receiver.try_recv() {
-            Ok(item) => Ok(Async::Ready(Some(item))),
+        println!();
+        println!(">> Stream::poll");
+        let ret = match self.receiver.try_recv() {
+            Ok((item, task)) => {
+                task.notify();
+                Ok(Async::Ready(Some(item)))
+            },
             Err(mpsc::TryRecvError::Empty) => Ok(Async::NotReady),
             Err(mpsc::TryRecvError::Disconnected) => Err(()),
-        }
+        };
+        println!("ret = {:?}", ret);
+        ret
     }
 }
+*/
 
 
 #[test]
@@ -77,7 +100,8 @@ fn basic_test_sender_receiver() {
     let get_cur_instant = || Instant::now();
 
     // We send tuples of (message_bytes, address)
-    let (sink, stream) = sink_stream_pair::<(Vec<u8>, u32)>(1);
+    // let (sink, stream) = sink_stream_pair::<(Vec<u8>, u32)>(1);
+    let (sink, stream) = mpsc::channel::<(Vec<u8>, u32)>(1);
 
     let frag_sender = FragMsgSender::new(sink, MAX_DGRAM_LEN, rng);
     let frag_receiver = FragMsgReceiver::new(stream, get_cur_instant);
@@ -92,15 +116,32 @@ fn basic_test_sender_receiver() {
         (b"".to_vec(), 0x2bcdef12)
     ];
 
-    let source_stream = stream::iter_ok(messages.iter().map(|&(ref msg, ref address)| (msg.clone(), address.clone())));
+    let source_stream = stream::iter_ok(messages.clone());
     let send_all = frag_sender.send_all(source_stream);
 
-    let mut core = Core::new().unwrap();
-    let (_frag_sender, _source_stream) = core.run(send_all).unwrap();
+    // Spawn a future that tries to read everything from the stream:
+    
+    let mut incoming_messages = Vec::new();
 
-    // TODO: Spawn a new task here for reading from the stream.
-    // Keep all the read messages, and finally compare them to the original messages.
+    {
+        let mut core = Core::new().unwrap();
+        let handle = core.handle();
 
-    assert!(false);
+        // Start a task to send all of our messages:
+        handle.spawn(send_all.then(|_| Ok(())));
+
+        let keep_messages = frag_receiver.for_each(|(message, address)| {
+            incoming_messages.push((message, address));
+            println!("Received message!");
+            Ok(())
+        });
+
+        // Read messages and keep them in a vector:
+        core.run(keep_messages).unwrap();
+    }
+
+    // Compare to original messages:
+    assert_eq!(incoming_messages, messages);
+
 }
 
